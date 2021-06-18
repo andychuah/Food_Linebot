f473666ce361f9a656c37ef7a3f9d0bcabcec869
Andy <70196193+andychuah@users.noreply.github.com>
2021-06-19 00:14:35 +0800

    Update settings.py

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -20,17 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#47ly5pw_-r-a(@$v1pbqn65-*17857mv48%($alh48a9)=_t#'
+SECRET_KEY = 'YOUR_DJANGO_SECRET_KEY'
 
-LINE_CHANNEL_ACCESS_TOKEN = 'bg0KekbKU8f2IC38h3S2iTDa+VhFROkFXZZAkJoaQ1TGcaf06ITNoCOkVOV3smtxkB6/IgM0aJG1HOdc5X6RqiE5fXM3Pa1MOupv/qLE9snOAxmAgOSHjOhQ7ym6sIR0u9VoOJYktrhaXj/G4wg6yQdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
  
-LINE_CHANNEL_SECRET = 'cfb7138a5c2460d209b3b4363d28fed1'
+LINE_CHANNEL_SECRET = 'YOUR_LINE_SECRETKEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '25c9a40b7b33.ngrok.io'
+    'NGROK_HTTPS'
 ]
 
 
